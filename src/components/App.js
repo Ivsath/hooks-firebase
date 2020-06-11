9797529a4cd4ebe73ab4d2c402d871c20d8bdd1d
Irvin Reyes <reyes.yerp@gmail.com>
2020-06-11 12:02:05 -0500

    Add routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
+import CreateLink from "./Link/CreateLink";
+import Login from "./Auth/Login";
+import ForgotPassword from "./Auth/ForgotPassword";
+import SearchLinks from "./Link/SearchLinks";
+import LinkList from "./Link/LinkList";
+import LinkDetail from "./Link/LinkDetail";
 
 function App() {
   return (
-    <div style={{ margin: "0 auto", textAlign: "center" }}>
-      <img style={{ height: "100vh" }} src="/logo.png" alt="Logo" />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" render={() => <Redirect to="/new/1" />} />
+        <Route path="/create" component={CreateLink} />
+        <Route path="/login" component={Login} />
+        <Route path="/forgot" component={ForgotPassword} />
+        <Route path="/search" component={SearchLinks} />
+        <Route path="/top" component={LinkList} />
+        <Route path="/new/:page" component={LinkList} />
+        <Route path="/link/:linkId" component={LinkDetail} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
