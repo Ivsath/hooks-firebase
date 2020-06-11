80bc4b6eec06cbd6a39b5d5c4ad91f69eaa8a73a
Irvin Reyes <reyes.yerp@gmail.com>
2020-06-11 13:05:36 -0500

    Add header and navigation

--- src/components/App.js
+++ src/components/App.js
@@ -7,20 +7,26 @@ import ForgotPassword from "./Auth/ForgotPassword";
 import SearchLinks from "./Link/SearchLinks";
 import LinkList from "./Link/LinkList";
 import LinkDetail from "./Link/LinkDetail";
+import Header from "./Header";
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" render={() => <Redirect to="/new/1" />} />
-        <Route path="/create" component={CreateLink} />
-        <Route path="/login" component={Login} />
-        <Route path="/forgot" component={ForgotPassword} />
-        <Route path="/search" component={SearchLinks} />
-        <Route path="/top" component={LinkList} />
-        <Route path="/new/:page" component={LinkList} />
-        <Route path="/link/:linkId" component={LinkDetail} />
-      </Switch>
+      <div className="app-container">
+        <Header />
+        <div className="route-container">
+          <Switch>
+            <Route exact path="/" render={() => <Redirect to="/new/1" />} />
+            <Route path="/create" component={CreateLink} />
+            <Route path="/login" component={Login} />
+            <Route path="/forgot" component={ForgotPassword} />
+            <Route path="/search" component={SearchLinks} />
+            <Route path="/top" component={LinkList} />
+            <Route path="/new/:page" component={LinkList} />
+            <Route path="/link/:linkId" component={LinkDetail} />
+          </Switch>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
