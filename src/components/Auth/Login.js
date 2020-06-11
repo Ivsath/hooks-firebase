1d79f420fb81b941412ae9402d730edfb045fc06
Irvin Reyes <reyes.yerp@gmail.com>
2020-06-11 17:32:26 -0500

    Create and toggle the login/register form

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login(props) {
-  return <div>Login</div>;
+  const [login, setLogin] = useState(true);
+
+  return (
+    <div>
+      <h2 className="mv3">{login ? "Login" : "Create Account"}</h2>
+      <form action="" className="flex flex-column">
+        {!login && (
+          <input type="text" placeholder="Your name" autoComplete="off" />
+        )}
+        <input type="email" placeholder="Your email" autoComplete="off" />
+        <input type="password" placeholder="Choose a secure password" />
+        <div className="flex mt3">
+          <button type="submit" className="button pointer mr2">
+            Submit
+          </button>
+          <button
+            type="button"
+            className="pointer button"
+            onClick={() => setLogin((prevLogin) => !prevLogin)}
+          >
+            {login ? "Need to create an account?" : "Already have an account?"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
