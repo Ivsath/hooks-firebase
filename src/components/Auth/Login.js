520cd8a6125c76404d6b00b5c9bc496f1e3b355b
Irvin Reyes <reyes.yerp@gmail.com>
2020-06-11 23:58:26 -0500

    Handle serverside errors

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -19,14 +19,18 @@ function Login(props) {
     isSubmitting,
   } = useFormValidation(INITIAL_STATE, validateLogin, authenticateUser);
   const [login, setLogin] = useState(true);
+  const [firebaseError, setFirebaseError] = useState(null);
 
   async function authenticateUser() {
     const { name, email, password } = values;
-    const response = login
-      ? await firebase.login(email, password)
-      : await firebase.register(name, email, password);
-
-    console.log(response);
+    try {
+      login
+        ? await firebase.login(email, password)
+        : await firebase.register(name, email, password);
+    } catch (err) {
+      console.log("Authentication Error", err);
+      setFirebaseError(err.message);
+    }
   }
 
   return (
@@ -64,6 +68,7 @@ function Login(props) {
           onBlur={handleBlur}
         />
         {errors.password && <p className="error-text">{errors.password}</p>}
+        {firebaseError && <p className="error-text">{firebaseError}</p>}
         <div className="flex mt3">
           <button
             type="submit"
