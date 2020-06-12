8d90b14602a664eee8c514e71acabc45892ed338
Irvin Reyes <reyes.yerp@gmail.com>
2020-06-12 12:08:09 -0500

    Add password reset functionality

--- src/components/Auth/ForgotPassword.js
+++ src/components/Auth/ForgotPassword.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { FirebaseContext } from "../../firebase";
 
 function ForgotPassword() {
-  return <div>ForgotPassword</div>;
+  const { firebase } = useContext(FirebaseContext);
+  const [resetPasswordEmail, setResetPasswordEmail] = useState("");
+  const [isPasswordReset, setIsPasswordReset] = useState(false);
+  const [passwordResetError, setPasswordResetError] = useState(null);
+
+  const handleResetPassword = async () => {
+    try {
+      await firebase.resetPassword(resetPasswordEmail);
+      setIsPasswordReset(true);
+      setPasswordResetError(null);
+    } catch (err) {
+      console.error("Error sending email", err);
+      setPasswordResetError(err.message);
+      setIsPasswordReset(false);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="email"
+        name="email"
+        className="input"
+        placeholder="Provide your account email"
+        onChange={(event) => setResetPasswordEmail(event.target.value)}
+      />
+      <div>
+        <button className="button" onClick={handleResetPassword}>
+          Reset Password
+        </button>
+      </div>
+      {isPasswordReset && <p>Check email to reset password</p>}
+      {passwordResetError && <p className="error-text">{passwordResetError}</p>}
+    </div>
+  );
 }
 
 export default ForgotPassword;
--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import useFormValidation from "./useFormValidation";
 import validateLogin from "./validateLogin";
 import firebase from "../../firebase";
@@ -88,6 +89,9 @@ function Login(props) {
           </button>
         </div>
       </form>
+      <div className="forgot-password">
+        <Link to="/forgot">Forgot password?</Link>
+      </div>
     </div>
   );
 }
