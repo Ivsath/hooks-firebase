423db596fec104ec83871ce777db7de980529425
Irvin Reyes <reyes.yerp@gmail.com>
2020-06-12 11:30:15 -0500

    Add logout functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,17 +23,29 @@ function Header() {
         <NavLink to="/search" className="header-link">
           search
         </NavLink>
-        <div className="divider">|</div>
-        <NavLink to="/create" className="header-link">
-          submit
-        </NavLink>
+        {user && (
+          <>
+            <div className="divider">|</div>
+            <NavLink to="/create" className="header-link">
+              submit
+            </NavLink>
+          </>
+        )}
       </div>
       <div className="flex">
         {user ? (
           <>
             <div className="header-name">{user.displayName}</div>
             <div className="divider">|</div>
-            <div className="header-button">logout</div>
+            <div
+              tabIndex="0"
+              role="button"
+              className="header-button"
+              onClick={() => firebase.logout()}
+              onKeyDown={() => firebase.logout()}
+            >
+              logout
+            </div>
           </>
         ) : (
           <NavLink to="/login" className="header-link">
