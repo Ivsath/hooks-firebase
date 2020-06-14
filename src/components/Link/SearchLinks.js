5bd0b227e4de60d9ecc769366a0b89df825f7676
Irvin Reyes <reyes.yerp@gmail.com>
2020-06-14 01:24:05 -0500

    Add text search

--- src/components/Link/SearchLinks.js
+++ src/components/Link/SearchLinks.js
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { FirebaseContext } from "../../firebase";
+
+import LinkItem from "./LinkItem";
 
 function SearchLinks() {
-  return <div>SearchLinks</div>;
+  const { firebase } = useContext(FirebaseContext);
+  const [filteredLinks, setFilteredLinks] = useState([]);
+  const [links, setLinks] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    getInitialLinks();
+    // eslint-disable-next-line
+  }, []);
+
+  function getInitialLinks() {
+    firebase.db
+      .collection("links")
+      .get()
+      .then((snapshot) => {
+        const links = snapshot.docs.map((doc) => {
+          return { id: doc.id, ...doc.data() };
+        });
+        setLinks(links);
+      });
+  }
+
+  function handleSearch(event) {
+    event.preventDefault();
+    const query = filter.toLowerCase();
+    const matchedLinks = links.filter((link) => {
+      return (
+        link.description.toLowerCase().includes(query) ||
+        link.url.toLowerCase().includes(query) ||
+        link.postedBy.name.toLowerCase().includes(query)
+      );
+    });
+
+    setFilteredLinks(matchedLinks);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSearch}>
+        <div>
+          Search <input onChange={(event) => setFilter(event.target.value)} />
+          <button>OK</button>
+        </div>
+      </form>
+      {filteredLinks.map((filteredLink, index) => (
+        <LinkItem
+          key={filteredLink.id}
+          showCount={false}
+          link={filteredLink}
+          index={index}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default SearchLinks;
